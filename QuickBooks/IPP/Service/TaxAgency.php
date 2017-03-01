7a3f7354cb3cde2995baba1713aab40085328870
Joe Song <jsong@song.agency>
2017-03-01 13:55:55 -0600

    Improved TaxAgency support.

--- QuickBooks/IPP/Service/TaxAgency.php
+++ QuickBooks/IPP/Service/TaxAgency.php
@@ -21,8 +21,33 @@ QuickBooks_Loader::load('/QuickBooks/IPP/Service.php');
 
 class QuickBooks_IPP_Service_TaxAgency extends QuickBooks_IPP_Service
 {
+	public function add($Context, $realmID, $Object)
+	{
+		return parent::_add($Context, $realmID, QuickBooks_IPP_IDS::RESOURCE_TAXAGENCY, $Object);
+	}
+
+	public function update($Context, $realmID, $IDType, $Object)
+	{
+		return parent::_update($Context, $realmID, QuickBooks_IPP_IDS::RESOURCE_TAXAGENCY, $Object, $IDType);
+	}
+
 	public function query($Context, $realm, $query)
 	{
 		return parent::_query($Context, $realm, $query);
+	}
+
+	public function delete($Context, $realmID, $IDType)
+	{
+		return parent::_delete($Context, $realmID, QuickBooks_IPP_IDS::RESOURCE_TAXAGENCY, $IDType);
+	}
+
+	public function void($Context, $realmID, $IDType)
+	{
+		return parent::_void($Context, $realmID, QuickBooks_IPP_IDS::RESOURCE_TAXAGENCY, $IDType);
+	}
+
+	public function pdf($Context, $realmID, $IDType)
+	{
+		return parent::_pdf($Context, $realmID, QuickBooks_IPP_IDS::RESOURCE_TAXAGENCY, $IDType);
 	}
 }
